d8d0d647d3edb81a1bd0f58bfced9610e6475c02
Muhammad Asim <47195175+muhamdasim@users.noreply.github.com>
2020-09-07 20:48:48 +0500

    Updates

--- caring_scraper.py
+++ caring_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 from itertools import islice
 
 
-#list to string
+# list to string
 
 def listToString(s):
     # initialize an empty string
@@ -27,27 +27,47 @@ def pageTitle(soup):
 
 def MetaDescription(soup):
     data = soup.find_all('meta')
-    return listToString([meta.attrs['content'] for meta in data if 'name' in meta.attrs and meta.attrs['name'] == 'description' ])
+    return listToString(
+        [meta.attrs['content'] for meta in data if 'name' in meta.attrs and meta.attrs['name'] == 'description'])
+
 
 def communityName(soup):
     return soup.find(itemprop='name').h1.text.strip()
 
+
 def getCommunityStreetAddress(soup):
-    return soup.find(class_='street-address').text.strip()+ "," + getCommunityCity(soup) +"," + getCommunityState(soup) + getCommunityZipCode(soup)
+    return soup.find(class_='street-address').text.strip() + "," + getCommunityCity(soup) + "," + getCommunityState(
+        soup) + getCommunityZipCode(soup)
+
 
 def getCommunityCity(soup):
     return soup.find(class_='locality').text.strip()
 
+
 def getCommunityState(soup):
     return soup.find(class_='region').text.strip()
+
 
 def getCommunityZipCode(soup):
     return soup.find(class_='postal-code').text.strip()
 
+
 def getCommunityImages(soup):
-    return
+    data = []
+    limit = int(len(soup.find_all(class_='item')) / 2)
+    for i in islice(soup.find_all(class_='item'), limit):
+        data.append(i.find('img').get('data-src'))
+
+    for i in data:
+        data += i + ","
+
+    return data[:-1]
+
+
+def getCommunityContent(soup):
+    content = soup.find('div', class_='section-content').get_text().strip()
+    print(content)
+
 
 def test(soup):
-    limit=int(len(soup.find_all(class_='item'))/2)
-    for i in islice(soup.find_all(class_='item'),limit):
-        print(i.find('img').get('data-src'))
+    return 0
